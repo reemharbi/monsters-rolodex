8a634e720edffe5ec9e1e53f586b747860d1233f
reemharbi <reemharbi.3@gmail.com>
2020-01-27 16:21:39 +0300

    Fetch API for monsters names

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        // {
+        //   name: "Dracula",
+        //   id: '123'
+        // },
+        // {
+        //   name: "Frankenstein",
+        //   id: '147'
+        // },
+        // {
+        //   name: "Zombie",
+        //   id: '159'
+        // }
+      ]
+    };
+  }
+
+// 
+componentDidMount(){
+  // Javascript native fetch to make an API request
+  fetch('https://jsonplaceholder.typicode.com/users')
+  // fetch returns a promise which gives us 
+  // the actual body of the response which we will
+  // turn into a json file so javascript can 
+  // understand and use it
+  .then(response => response.json())
+  // make another promise
+  // then take the users we got from the response
+  // and set our monsters array to that
+  // .then(users => console.log(users))
+  .then(users => this.setState({monsters: users}));
 }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
