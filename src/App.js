fa821e7e4f8d4e87a5c4af35eb6b6e03a5dcbd63
reemharbi <reemharbi.3@gmail.com>
2020-01-27 18:04:45 +0300

    Filtered Search

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export default class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: []
+            monsters: [],
+            searchInput: ''
         };
     }
 
@@ -19,18 +20,30 @@ export default class App extends Component {
             // the actual body of the response which we will
             // turn into a json file so javascript can
             // understand and use it
-            .then(response => response.json())
+            .then((response) => response.json())
             // make another promise
             // then take the users we got from the response
             // and set our monsters array to that
             // .then(users => console.log(users))
-            .then(users => this.setState({ monsters: users }));
+            .then((users) => this.setState({ monsters: users }));
     }
 
     render() {
+        const { monsters, searchInput } = this.state;
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchInput.toLowerCase())
+        );
+        
         return (
             <div className='App'>
-                <CardList monsters={this.state.monsters} />
+                <input
+                    type='search'
+                    placeholder='search monsters'
+                    onChange={(e) =>
+                        this.setState({ searchInput: e.target.value })
+                    }
+                />
+                <CardList monsters={filteredMonsters} />
             </div>
         );
     }
