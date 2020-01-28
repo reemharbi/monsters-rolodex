7cf619f316a4154efa18ab6d623d123da009d292
reemharbi <reemharbi.3@gmail.com>
2020-01-28 13:23:10 +0300

    Updates

---
+++ precache-manifest.5d01dd40cca79ee9393a36b20356133b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3321a0f55a4b2b533a495e871c4875b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2cc3bfee62a395890228",
+    "url": "/monsters-rolodex/static/css/main.21f7c039.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cc3bfee62a395890228",
+    "url": "/monsters-rolodex/static/js/main.bf64d6bd.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
